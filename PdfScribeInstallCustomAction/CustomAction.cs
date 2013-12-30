8a7606a7c38e07373a45e830dae9880dad79f814
S T Chan <sherman.chan@pobox.com>
2013-12-30 14:39:33 -0500

    custom action changes

--- PdfScribeInstallCustomAction/CustomAction.cs
+++ PdfScribeInstallCustomAction/CustomAction.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Text;
 using Microsoft.Deployment.WindowsInstaller;
 
+using PdfScribeCore;
+
 namespace PdfScribeInstallCustomAction
 {
     public class CustomActions
     {
-        [CustomAction]
-        public static ActionResult CustomAction1(Session session)
-        {
-            session.Log("Begin CustomAction1");
 
-            return ActionResult.Success;
+        [CustomAction]
+        public static ActionResult CheckIfPrinterNotInstalled(Session session)
+        {
+            ActionResult resultCode;
+
+            PdfScribeInstaller installer = new PdfScribeInstaller();
+            if (installer.IsPdfScribePrinterInstalled())
+            {
+                resultCode = ActionResult.Success;
+            }
+            else
+            {
+                resultCode = ActionResult.Failure;
+            }
+
+            return resultCode;
         }
     }
 }
