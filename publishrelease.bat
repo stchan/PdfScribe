566395ca631569a4f4c7a8ad945189ab9d688806
Linda Mennega <99175969+lm-ubunye@users.noreply.github.com>
2024-08-28 11:11:26 +0200

    Remove signing

--- publishrelease.bat
+++ publishrelease.bat
@@ -2,11 +2,6 @@ if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools
     call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
     echo on
     msbuild PdfScribe.sln  -t:Rebuild -p:Configuration=Release -r
-    if NOT EXIST "publish\signed" mkdir "publish\signed"
-    del /q "publish\signed\*"
-    copy /y  "publish\unsigned\PdfScribeInstall_*.msi" "publish\signed\"
-    cd "publish\signed\"
-    signtool sign /sha1 %CodeSignHash% /t http://time.certum.pl /fd sha256 /v PdfScribeInstall_*.msi
 ) ELSE (
     echo Could not set build tools environment.
     exit 1
